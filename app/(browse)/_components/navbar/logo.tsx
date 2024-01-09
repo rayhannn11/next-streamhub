4a87817b87abada11dd74cbf5888563e368d0d18
rayhannn11 <rayhan.onepiece@gmail.com>
2024-01-09 11:21:02 +0700

    change logo

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -13,8 +13,8 @@ export const Logo = () => {
   return (
     <Link href='/'>
       <div className='flex items-center gap-x-4 hover:opacity-75 transition'>
-        <div className='bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink'>
-          <Image src='/spooky.svg' alt='Gamehub' height='32' width='32' />
+        <div className='rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink'>
+          <Image src='/logo.png' alt='Streamhub' height='32' width='32' />
         </div>
         <div className={cn('hidden lg:block', font.className)}>
           <p className='text-lg font-semibold'>Streamhub</p>
